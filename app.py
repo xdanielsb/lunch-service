e4f9bf6a32dbed23d854d758e1e5b1662142ae21
Daniel <dfsantosbu@unal.edu.co>
2020-11-19 17:18:09 +0100

    fix: location connection and conf.

--- app.py
+++ app.py
@@ -1,31 +1,13 @@
-import psycopg2
 from flask import request
 from flask import Flask, render_template
 from control.dao.user import User
 import os
 import json
+from control.connection import Connection
 
 app = Flask(__name__)
-# app.config.from_object("config")
+app.config.from_object("config.Config")
 
-
-class Connection:
-    def __init__(self):
-        self.conn = psycopg2.connect(
-            host="localhost", database="apoyo_alimentario", user="postgres", password=""
-        )
-
-    def query(self, query):
-        cur = self.conn.cursor()
-        cur.execute(query)
-        ans = cur.fetchone()
-        cur.close()
-        return ans
-
-    def execute(self, statement):
-        cur = self.conn.cursor()
-        cur.execute(query)
-        cur.close()
 
 
 conn = Connection()
@@ -34,7 +16,7 @@ userDao = User(conn)
 @app.route("/login",methods = ['POST', 'GET'])
 def login():
     if request.method == "POST":
-        if(userDao.exist(request.form['username'], request.form['password']):
+        if(userDao.exist(request.form['username'], request.form['password'])):
             return render_template('home.html')
     return render_template('login.html')
 
--- control/connection.py
+++ control/connection.py
@@ -1,1 +1,21 @@
+import psycopg2
+
+class Connection:
+    def __init__(self):
+        self.conn = psycopg2.connect(
+            host="localhost", database="apoyo_alimentario", user="postgres", password=""
+        )
+
+    def query(self, query):
+        cur = self.conn.cursor()
+        cur.execute(query)
+        ans = cur.fetchone()
+        cur.close()
+        return ans
+
+    def execute(self, statement):
+        cur = self.conn.cursor()
+        cur.execute(query)
+        cur.close()
+
 
