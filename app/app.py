ef3de4f2a389f0fba56b8e820a7ce68d72c2bb1e
Daniel <dfsantosbu@unal.edu.co>
2020-11-19 23:52:47 +0100

    feat: add logic to upload files.

--- app/app.py
+++ app/app.py
@@ -15,6 +15,20 @@ from control.connection import get_db, query, execute
 
 app = Flask(__name__)
 app.config.from_object("config.Config")
+# max file size 16MB
+app.config["MAX_CONTENT_LENGTH"] = 16 * 1024 * 1024
+
+path = os.getcwd()
+UPLOAD_FOLDER = os.path.join(path, "uploads")
+if not os.path.isdir(UPLOAD_FOLDER):
+    os.mkdir(UPLOAD_FOLDER)
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+ALLOWED_EXTENSIONS = set(["txt", "pdf", "png", "jpg", "jpeg"])
+
+
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.before_request
@@ -72,11 +86,24 @@ def home():
 @app.route("/solicitud")
 @login_required
 def solicitud():
+    if request.method == "POST":
+        if "files[]" not in request.files:
+            flash("No file part")
+            return redirect(request.url)
+        files = request.files.getlist("files[]")
+        for file in files:
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+        flash("File(s) successfully uploaded")
+        return redirect(url_for("home"))
+
     convocatoria = Convocatoria()
     today = date.today()
     if convocatoria.is_active(fecha_actual=today.strftime("%Y-%m-%d")) is not True:
         flash("No hay convocatoria activa.")
         return redirect(url_for("home"))
+
     return render_template("solicitud.html")
 
 
